bb9b1c894fa18ac69cab7ecbbb6562c05b3fb1ad
shayan ghazali <shayan97.tti@gmail.com>
2023-06-16 11:56:48 +0430

    login page done

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  customerForm = new FormGroup({
+    username: new FormControl({ value: '', disabled: false }, [Validators.required, Validators.pattern(/^[a-z0-9]{3,12}$/i)]),
+    password: new FormControl({ value: '', disabled: false }, [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%&*!])[A-Za-z\d@#$%&*!]{6,12}$/)]),
+  });
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  submit(){
+    if (this.customerForm.invalid) {
+      return;
+    }
+
+  }
 }
