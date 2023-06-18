fba58b585dfcb448595dc6fd5d28c13b53a45d8c
shayan ghazali <shayan97.tti@gmail.com>
2023-06-18 23:55:28 +0430

    define a model for auth state

--- src/app/shared/models/models.ts
+++ src/app/shared/models/models.ts
@@ -1,3 +1,11 @@
 export interface CurrentUser{
-    
+    Key: number;
+    Value: {
+        Token: string;
+        RefreshToken: string;
+        SrName: string;
+        SrFamil: string;
+        expireMinutes: string;
+    }
 }
+export type AuthState = CurrentUser | null;
