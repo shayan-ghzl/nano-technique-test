2c1d0cc7bd629da37ee73e101137fd45aaefafee
shayan ghazali <shayan97.tti@gmail.com>
2023-06-19 12:09:50 +0430

    create a separate componnet for score upload image

---
+++ src/app/shared/components/score-upload-image/score-upload-image.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { ScoreUploadImageComponent } from './score-upload-image.component';
+
+describe('ScoreUploadImageComponent', () => {
+  let component: ScoreUploadImageComponent;
+  let fixture: ComponentFixture<ScoreUploadImageComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ScoreUploadImageComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ScoreUploadImageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/score-upload-image/score-upload-image.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Camera, CameraResultType } from '@capacitor/camera';
+
+@Component({
+  selector: 'app-score-upload-image',
+  templateUrl: './score-upload-image.component.html',
+  styleUrls: ['./score-upload-image.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: ScoreUploadImageComponent
+    }
+  ]
+})
+export class ScoreUploadImageComponent implements ControlValueAccessor {
+
+  imagesInterface: string[] = ['', '', '', '', ''];
+
+  images: string[] = [];
+
+  onChange = (images: string[]) => {};
+  onTouched = () => {};
+  touched = false;
+  disabled = false;
+
+  writeValue(obj: string[]): void {
+    this.images = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+
+  async selectImage(index: number) {
+    this.markAsTouched();
+    if (!this.disabled) {
+      const image = await Camera.getPhoto({
+        quality: 100,
+        allowEditing: false,
+        resultType: CameraResultType.Uri
+      });
+      if (image.webPath) {
+        this.images.push(image.webPath);
+        this.imagesInterface[index] = image.webPath;
+      }
+      this.onChange(this.images);
+    }
+  }
+
+}
