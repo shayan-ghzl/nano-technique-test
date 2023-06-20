d11aae35bba9907fe3fb9bc48e826f4a7f3f3154
shayan ghazali <shayan97.tti@gmail.com>
2023-06-20 17:27:22 +0430

    single page need some requests to call

--- src/app/single-action/single-action.page.ts
+++ src/app/single-action/single-action.page.ts
@@ -9,52 +9,46 @@ import { Subscription } from 'rxjs';
 })
 export class SingleActionPage implements OnInit, OnDestroy {
 
+
+  hasActionStarted = false;
+
   alertButtons = [
     {
       text: 'بله',
       role: 'confirm',
       handler: () => {
-        return Promise.resolve(true);
+        this.hasActionStarted = true;
       },
     },
     {
       text: 'خیر',
-      role: 'cancel',
-      handler: () => {
-        return Promise.resolve(true);
-      },
+      role: 'cancel'
     }
   ];
 
   subscription = new Subscription();
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
-    this.subscription.add(
-      this.activatedRoute.paramMap.subscribe(params => {
-        const id = params.get('actionId'); // get the ID parameter from the URL
-        console.log(id); // log the ID value to the console
-      })
-    );
-    // or
-    console.log(this.activatedRoute.snapshot.paramMap.get('actionId'));
-    // -----------------
-    // using state
     // this.subscription.add(
-    //   this.activatedRoute.data.subscribe(params => {
-    //     this.product = params['product'].data;
-    //     console.log(this.product);
+    //   this.activatedRoute.paramMap.subscribe(params => {
+    //     console.log(params.get('actionId')); 
     //   })
     // );
-    // or
-    // this.activatedRoute.paramMap.pipe(
-    //   map(() => window.history.state),
-    // );
+  }
+      
+  ionViewWillEnter() {
+    console.log(this.activatedRoute.snapshot.paramMap.get('actionId'));
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
   }
+
+  ionViewDidLeave() {
+    this.subscription.unsubscribe();
+  } 
 
 }
