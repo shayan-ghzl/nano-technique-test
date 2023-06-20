4a2fec47ef233417061abacb6cbb3e58885d4076
shayan ghazali <shayan97.tti@gmail.com>
2023-06-20 17:25:44 +0430

    there is no item to get also pagination needed

--- src/app/monthly-report/monthly-report.page.ts
+++ src/app/monthly-report/monthly-report.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Subscription, switchMap, take, tap } from 'rxjs';
+import { AuthenticationService } from '../shared/services/authentication.service';
+import { ApiService } from '../shared/services/api.service';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
+import * as pako from 'pako';
 
 @Component({
   selector: 'app-monthly-report',
@@ -7,16 +12,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonthlyReportPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('refresher') refresher!: HTMLIonRefresherElement;
+
+  items: any[] | null = null;
+  showToastError = false;
+
+  subscription = new Subscription();
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private apiService: ApiService,
+  ) { }
 
   ngOnInit() {
   }
 
-  handleRefresh(event: any) {
-    setTimeout(() => {
-      // Any calls to load data go here
-      event.target.complete();
-    }, 5000);
+  ionViewWillEnter() {
+    this.getReports();
   }
 
+  getReports(fromRefresher: boolean = false){
+    // @ts-ignore:
+    const [today, monthAgo]: [string, string] = [new Date().nanoFormat(), new Date().daysAgo(31).nanoFormat()];
+
+    this.subscription.add(
+      this.authenticationService.getAuthState$.pipe(
+        switchMap(authState => {
+          return this.apiService.getinstalledDevices({ 'ServicerID': authState!.Key, 'AzDateCTI': '2023-01-01', 'TaDateCTI': '2023-06-30', 'Skip': -1 })
+        })
+      ).pipe(
+        take(1),
+        tap(response => {
+          if (response) {
+            if (response.RData) {
+              const compressed = pako.deflate(response.RData);
+              const restored = JSON.parse(pako.inflate(compressed, { to: 'string' }));
+              this.items = restored;
+            } else {
+              this.items = [];
+            }
+          } else {
+            this.showToastError = true;
+          }
+        })
+      ).subscribe({
+        complete: () => {
+          if (fromRefresher) {
+            this.refresher.complete()
+          }
+        },
+       })
+    );
+  }
+
+  handleRefresh() {
+    this.getReports(true);
+  }
+
+  onIonInfinite(ev: any) {
+    this.getReports();
+    
+    (ev as InfiniteScrollCustomEvent).target.complete();
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  ionViewDidLeave() {
+    this.subscription.unsubscribe();
+  }
 }
