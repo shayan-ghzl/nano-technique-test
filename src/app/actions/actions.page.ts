38fc27d9ff08b3437bd98b8654cbbdc3a0168abe
shayan ghazali <shayan97.tti@gmail.com>
2023-06-30 22:24:54 +0430

    large changes commited from home to work

--- src/app/actions/actions.page.ts
+++ src/app/actions/actions.page.ts
@@ -1,21 +1,32 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { ApiService } from '../shared/services/api.service';
-import { Subscription, switchMap, take, tap } from 'rxjs';
-import { AuthenticationService } from '../shared/services/authentication.service';
-import { InfiniteScrollCustomEvent } from '@ionic/angular';
 import * as pako from 'pako';
+import { EMPTY, Subscription, mergeMap, of, take } from 'rxjs';
+import { ApiService } from '../shared/services/api.service';
+import { AuthenticationService } from '../shared/services/authentication.service';
+
+enum FROMSTATE {
+  REFRESHER = 1,
+  INFINITESCROLL = 2,
+}
 
 @Component({
   selector: 'app-actions',
   templateUrl: './actions.page.html',
   styleUrls: ['./actions.page.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ActionsPage implements OnInit, OnDestroy {
 
   @ViewChild('refresher') refresher!: HTMLIonRefresherElement;
+  @ViewChild('infiniteScroll') infiniteSctoll!: HTMLIonInfiniteScrollElement;
 
   items: any[] | null = null;
+
   showToastError = false;
+  toastMessage = '';
+
+  PageNum = 1;
+  totalPages = 1;
 
   subscription = new Subscription();
 
@@ -32,50 +43,68 @@ export class ActionsPage implements OnInit, OnDestroy {
     this.getActions();
   }
 
-  getActions(fromRefresher: boolean = false){
+  getActions(state: FROMSTATE | null = null){
    // @ts-ignore:
    const [today, monthAgo]: [string, string] = [new Date().nanoFormat(), new Date().daysAgo(31).nanoFormat()];
 
    this.subscription.add(
      this.authenticationService.getAuthState$.pipe(
-       switchMap(authState => {
-         return this.apiService.getReadyDevices({ 'ServicerID': authState!.Key, 'AzDateCTI': '2023-01-01', 'TaDateCTI': '2023-06-30', 'Skip': -1 })
-       })
-     ).pipe(
        take(1),
-       tap(response => {
-         if (response) {
-           if (response.RData) {
-             const compressed = pako.deflate(response.RData);
-             const restored = JSON.parse(pako.inflate(compressed, { to: 'string' }));
-             this.items = restored;
-             console.log(this.items);
-             
-           } else {
-             this.items = [];
-           }
-         } else {
-           this.showToastError = true;
-         }
+       mergeMap(authState => {
+         return this.apiService.getReadyDevices({ 'ServicerID': authState!.Key, 'AzDateCTI': '2023-01-01', 'TaDateCTI': '2023-06-30', 'Skip': -1, 'PageNum': this.PageNum }).pipe(
+          mergeMap((response) => {
+            let restored: any[] = [];
+            if (response) {
+              if (response.RData) {
+                const compressed = pako.deflate(response.RData);
+                restored = JSON.parse(pako.inflate(compressed, { to: 'string' }));
+                if (state === FROMSTATE.REFRESHER) {
+                  this.items = restored;
+                } else {
+                  this.items = (this.items) ? this.items.concat(restored) : restored;
+                }
+                this.totalPages = response.totalPages;
+              } else {
+                this.items = [];
+              }
+            } else {
+              this.toastMessage = 'خطایی رخ داد لطفا دوباره امتحان کنید.';
+              this.showToastError = true;
+            }
+            if (state === FROMSTATE.REFRESHER) {
+              this.refresher.complete()
+            } else if (state === FROMSTATE.INFINITESCROLL) {
+              this.infiniteSctoll.complete();
+            }
+            if (restored.length) {
+              return this.apiService.postViewdPlans({ 'ServicerID': authState!.Key, 'json_IDRList': JSON.stringify(restored.map(x => ({ 'idrRid': x.idrRid }))) });
+            } 
+            return EMPTY;
+           })
+          )
        })
-     ).subscribe({
-      complete: () => {
-        if (fromRefresher) {
-          this.refresher.complete()
-        }
-      },
-     })
+     ).subscribe()
    );
   }
 
   handleRefresh() {
-    this.getActions(true);
+    this.PageNum = 1;
+    this.getActions(FROMSTATE.REFRESHER);
   }
 
-  onIonInfinite(ev: any) {
-    this.getActions();
-    
-    (ev as InfiniteScrollCustomEvent).target.complete();
+  onIonInfinite() {
+    if (this.totalPages > this.PageNum) {
+      this.PageNum = this.PageNum + 1;
+      this.getActions(FROMSTATE.INFINITESCROLL);
+    } else {
+      this.infiniteSctoll.complete();
+    }
+  }
+
+  actionActived(item: any) {
+   console.log(item);
+   this.toastMessage = 'نصب این دستگاه به شما واگذار شد.';
+   this.showToastError = true;
   }
 
   ngOnDestroy(): void {
--- src/app/monthly-report/monthly-report.page.ts
+++ src/app/monthly-report/monthly-report.page.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Subscription, switchMap, take, tap } from 'rxjs';
-import { AuthenticationService } from '../shared/services/authentication.service';
-import { ApiService } from '../shared/services/api.service';
-import { InfiniteScrollCustomEvent } from '@ionic/angular';
 import * as pako from 'pako';
+import { Subscription, switchMap, take, tap } from 'rxjs';
+import { ApiService } from '../shared/services/api.service';
+import { AuthenticationService } from '../shared/services/authentication.service';
+
+enum FROMSTATE {
+  REFRESHER = 1,
+  INFINITESCROLL = 2,
+}
 
 @Component({
   selector: 'app-monthly-report',
   templateUrl: './monthly-report.page.html',
   styleUrls: ['./monthly-report.page.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MonthlyReportPage implements OnInit {
 
   @ViewChild('refresher') refresher!: HTMLIonRefresherElement;
+  @ViewChild('infiniteScroll') infiniteSctoll!: HTMLIonInfiniteScrollElement;
 
   items: any[] | null = null;
   showToastError = false;
+  PageNum = 1;
+  totalPages = 1;
 
   subscription = new Subscription();
 
@@ -31,14 +39,14 @@ export class MonthlyReportPage implements OnInit {
     this.getReports();
   }
 
-  getReports(fromRefresher: boolean = false){
+  getReports(state: FROMSTATE | null = null){
     // @ts-ignore:
     const [today, monthAgo]: [string, string] = [new Date().nanoFormat(), new Date().daysAgo(31).nanoFormat()];
 
     this.subscription.add(
       this.authenticationService.getAuthState$.pipe(
         switchMap(authState => {
-          return this.apiService.getinstalledDevices({ 'ServicerID': authState!.Key, 'AzDateCTI': '2023-01-01', 'TaDateCTI': '2023-06-30', 'Skip': -1 })
+          return this.apiService.getinstalledDevices({ 'ServicerID': authState!.Key, 'AzDateCTI': '2023-01-01', 'TaDateCTI': '2023-06-30', 'Skip': -1, 'PageNum': this.PageNum })
         })
       ).pipe(
         take(1),
@@ -48,6 +56,7 @@ export class MonthlyReportPage implements OnInit {
               const compressed = pako.deflate(response.RData);
               const restored = JSON.parse(pako.inflate(compressed, { to: 'string' }));
               this.items = restored;
+              this.totalPages = response.totalPages;
             } else {
               this.items = [];
             }
@@ -57,8 +66,10 @@ export class MonthlyReportPage implements OnInit {
         })
       ).subscribe({
         complete: () => {
-          if (fromRefresher) {
+          if (state === FROMSTATE.REFRESHER) {
             this.refresher.complete()
+          } else if (state === FROMSTATE.INFINITESCROLL) {
+            this.infiniteSctoll.complete();
           }
         },
        })
@@ -66,13 +77,16 @@ export class MonthlyReportPage implements OnInit {
   }
 
   handleRefresh() {
-    this.getReports(true);
+    this.getReports(FROMSTATE.REFRESHER);
   }
 
-  onIonInfinite(ev: any) {
-    this.getReports();
-    
-    (ev as InfiniteScrollCustomEvent).target.complete();
+  onIonInfinite() {
+    if (this.totalPages > this.PageNum) {
+      this.PageNum = this.PageNum + 1;
+      this.getReports(FROMSTATE.INFINITESCROLL);
+    } else {
+      this.infiniteSctoll.complete();
+    }
   }
 
   ngOnDestroy(): void {
